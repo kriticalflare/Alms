c003debc5595a30a2edcb2d53f596caf08256151
unknown <krithiksuvarna@gmail.com>
2019-02-23 17:59:13 +0530

    raise the interactiveness up a notch

--- app/src/main/java/com/solidfrost/android/alms/Activity3.java
+++ app/src/main/java/com/solidfrost/android/alms/Activity3.java
@@ -3,11 +3,11 @@ package com.solidfrost.android.alms;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class organisations extends AppCompatActivity {
+public class Activity3 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_organisations);
+        setContentView(R.layout.activity_3);
     }
 }
--- app/src/main/java/com/solidfrost/android/alms/MainActivity.java
+++ app/src/main/java/com/solidfrost/android/alms/MainActivity.java
@@ -8,10 +8,19 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
         private Button button;
+        private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button2 =(Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity3();
+            }
+        });
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
     }
     public void openActivity2(){
         Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, Activity3.class);
         startActivity(intent);
     }
 }
